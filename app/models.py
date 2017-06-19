0d2da372831098de87999c0d05351a78f8b6bfd9
Andretalik <adrianadero.otieno@gmail.com>
2017-06-19 15:12:24 +0300

    [Fix #147100161] Fix the relationship between the User and Bucketlist

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     password_encrypt = db.Column(db.String(200))
-    bucketlist = db.Relationship('Bucketlist', backref='user')
+    bucketlist = db.relationship('Bucketlist', backref='user')
 
     def save(self):
         db.session.add(self)
