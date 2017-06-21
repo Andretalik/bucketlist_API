198f1c75a9ac33073afd8df455897db4622f1e2f
Andretalik <adrianadero.otieno@gmail.com>
2017-06-22 00:19:28 +0300

    [Chore #147177629] Fix the database layout
     - Add the required columns of the email, username and password to database
     - Add the column for the boolean of whether an item has been done or not
     - Fix the lengths of strings accepted by the database

--- app/models.py
+++ app/models.py
@@ -8,13 +8,13 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True, nullable=False)
-    email = db.Column(db.String(60), unique=True, nullable=False)
+    name = db.Column(db.String(120), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(200), nullable=False)
     bucketlist = db.relationship('Bucketlist', backref='users')
 
-    def __init__(self, name, email, password):
-        """Initialize the users with their names, emails and password"""
+    def __init__(self, name=None, email=None, password=None):
+        """Initialize the parameters belonging to the user"""
         self.name = name
         self.email = email
         self.password = Bcrypt().generate_password_hash(password).decode()
@@ -34,7 +34,7 @@ class Bucketlist(db.Model):
     __tablename__ = 'bucketlists'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
+    name = db.Column(db.String(100), nullable=False)
     creator = db.Column(db.Integer, db.ForeignKey('users.id'))
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
@@ -66,27 +66,9 @@ class Item(db.Model):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(255))
+    name = db.Column(db.String(100))
     bucketlist_owner = db.Column(db.Integer, db.ForeignKey('bucketlists.id'))
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
                               onupdate=db.func.current_timestamp())
-
-    def __init__(self, name):
-        """Initialize with name"""
-        self.name = name
-
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-
-    @staticmethod
-    def get_all():
-        return Item.query.all()
-
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-
-    def __repr__(self):
-        return "<Item: {}>".format(self.name)
+    done = db.Column(db.Boolean, default=False)
