1d1e8d961b2e72b37bd462ad38b1be21d1b910c2
Andretalik <adrianadero.otieno@gmail.com>
2017-07-03 15:54:55 +0300

    [Feature #147177629] Add token-based authorisation
     - Add function for log in that assigns a token to each user, which is required to access the rest of the functionalities
     - Add error handlers for 404 and 500 errors
     - Add use of Marshmallow validation of the user's credentials on both registration and login

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from flask import request, jsonify, abort
+from functools import wraps
+from flask_bcrypt import Bcrypt
 from instance.config import app_config
 
 
 db = SQLAlchemy()
+validation_schema = Marshmallow()
 
-from app.models import User
+from app.models import User, UserSchema
 
 
 def create_app(config_name):
@@ -18,15 +22,43 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+    validation_schema.init_app(app)
+
+    @app.errorhandler(404)
+    def unknown_page(e):
+        response = jsonify({"error": "Resource not found"})
+        response.status_code = 404
+        return response
+
+    @app.errorhandler(500)
+    def internal_server_error(e):
+        response = jsonify({"error": "The basis of the request is incorrect"})
+        response.status_code = 500
+        return response
+
+    def check_token(func):
+        @wraps(func)
+        def required_token(*args, **kwargs):
+            if request.headers.get("Authorization"):
+                token = bytes(request.headers.get("Authorization").
+                              split(" ")[1], "utf-8")
+                user_id = User.decode_access_token(token)
+                if user_id and isinstance(user_id, int):
+                    return func(*args, **kwargs)
+                else:
+                    return jsonify({"msg": "Access denied."})
+            else:
+                return jsonify({"msg": "Access denied."})
+        return required_token
 
     @app.route('/auth/register', methods=['POST'])
     def register():
         """This function registers the user who will create the bucketlists"""
-        name = str(request.data.get('name', ''))
+        username = str(request.data.get('username', ''))
         email = str(request.data.get('email', ''))
         password = str(request.data.get('password', ''))
-        if name:
-            if User.query.filter_by(name=name).first():
+        if username:
+            if User.query.filter_by(username=username).first():
                 response = jsonify({'msg': "Username unavailable"})
                 response.status_code = 200
                 return response
@@ -36,7 +68,15 @@ def create_app(config_name):
                     response.status_code = 200
                     return response
                 if password:
-                    user = User(name=name, email=email, password=password)
+                    print(username, email, password)
+                    errors = UserSchema().validate({"username": username,
+                                                    "email": email,
+                                                    "password": password},
+                                                   partial=True)
+                    if errors:
+                        return jsonify(errors)
+                    user = User(username=username, email=email,
+                                password=password)
                     user.save()
                     response = jsonify({'msg':
                                         "User has been created successfully"})
@@ -58,7 +98,31 @@ def create_app(config_name):
             response.status_code = 200
             return response
 
-    @app.route('/bucketlists/', methods=['POST', 'GET'])
+    @app.route('/auth/login', methods=['POST'])
+    def login():
+        """This function handles the log in process of a registered
+         user"""
+        username = request.data.get('username', False)
+        password = request.data.get('password', False)
+        if username and password:
+            errors = UserSchema().validate({"username": username,
+                                           "password": password},
+                                           partial=True)
+            if errors:
+                return errors
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if Bcrypt().check_password_hash(user.password, password):
+                access_token = User.create_access_token(user.id)
+                return jsonify({"token": access_token.decode()})
+        else:
+            response = jsonify({"msg": "Username and password invalid."})
+            response.status_code = 401
+        return username
+
+    @app.route('/bucketlists', methods=['POST', 'GET'])
+    @check_token
     def bucketlists():
         """This function does the actual creation of the bucketlist within the
         API"""
@@ -95,6 +159,7 @@ def create_app(config_name):
             return response
 
     @app.route('/bucketlists/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+    @check_token
     def bucketlist_management(id, **kwargs):
         """This function handles all the management functions of the bucketlist
         """
